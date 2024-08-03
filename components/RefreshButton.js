4021b4645e2aeae8af246be625cc166152b0ac8e
Josh Edson <joshedson@gmail.com>
2024-08-03 16:46:33 -0700

    Created refresh button component

---
+++ components/RefreshButton.js
@@ -0,0 +1,32 @@
+import {View, Pressable, StyleSheet} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function RefreshButton({onPress}) {
+    return (
+
+        <View style={styles.refreshButtonContainer}>
+            <Pressable  onPress = {onPress} style={styles.refreshButton}>
+                <Ionicons name="reload" size={38} color='rgb(31, 31, 31)' />
+           </Pressable>
+        </View>
+    );
+}
+
+const styles= StyleSheet.create ({
+    refreshButtonContainer: {
+        width:84,
+        height:84,
+        marginHorizontal:60,
+        borderWidth:4,
+        borderColor:'rgb(102,211,110)',
+        borderRadius:42,
+        padding:3,
+    },
+    refreshButton: {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:42,
+        backgroundColor:'#fff',
+    },
+})
